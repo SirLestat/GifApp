7c9196867a38380318996c59212e29c1a69e9677
Edgar Alejandro Santiago Perez <alejandrosantiago.cfeteit@gmail.com>
2024-10-14 19:39:38 -0600

    New Component form

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,9 +1,24 @@
-
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifApp = () => {
+  const [categories, setCategories] = useState(["One Piece", "Dr Stone"]);
+
+  const handleAddCategory = () => {
+    //const newCategory = "Shigatsu Kimi No Uso";
+    setCategories(["Shigatsu Kimi No Uso", ...categories]);
+  };
+
   return (
     <>
       <h1>GifApp</h1>
+      <AddCategory setCategories={setCategories} />
+      
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };

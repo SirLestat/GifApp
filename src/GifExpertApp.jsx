473f2d880e91f5e7caf20d0e787c7dad0bc0821e
Edgar Alejandro Santiago Perez <alejandrosantiago.cfeteit@gmail.com>
2024-11-06 19:06:04 -0600

    4

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["one piece"]);
+  const [categories, setCategories] = useState(["One Piece", "Steins gate "]);
 
-  console.log(categories);
+  const onAddCategory = (newCategory) => {
+    setCategories((prevCategories) => {
+      return [newCategory.toLowerCase(), ...prevCategories];
+    });
+  };
   return (
     <>
       {/*Titulo */}
       <h1>GifExpertApp</h1>
 
+      {/*Input*/}
+      <AddCategory onAddCategory={onAddCategory} />
+
       {/*Listado de gif */}
 
       <ol>
-        <li>a</li>
-        <li>b</li>
+        {/* */}
+        <button onClick={onAddCategory}>Agregar</button>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
       </ol>
 
       {/*Gif item */}
